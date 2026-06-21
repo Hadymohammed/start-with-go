df7f313367f05d371b8c02eeec888356addac1dc
Abdelhady <hadymohamed2002@gmail.com>
2026-06-21 23:17:55 +0300

    add logs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,23 @@
 package config
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
 
 type Config struct {
-    DatabaseURL string `envconfig:"DATABASE_URL" default:"postgres://postgres:postgres@localhost:5432/notes?sslmode=disable"`
-    APIKey      string `envconfig:"API_KEY" required:"true"`
-    Port        string `envconfig:"PORT" default:"8080"`
+	DatabaseURL string `envconfig:"DATABASE_URL" default:"postgres://postgres:postgres@localhost:5432/notes?sslmode=disable"`
+	APIKey      string `envconfig:"API_KEY" required:"true"`
+	Port        string `envconfig:"PORT" default:"8080"`
 }
 
 func Load() (Config, error) {
-    var cfg Config
-    if err := envconfig.Process("", &cfg); err != nil {
-        return Config{}, err
-    }
-    return cfg, nil
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return Config{}, err
+	}
+
+	log.Printf("Config loaded")
+	return cfg, nil
 }
